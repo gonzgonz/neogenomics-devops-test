2c178afb37c30bc66279e4110ec80e07e9da0fc8
Gonzalo Arce <gonzaloarce.uk@gmail.com>
2025-02-20 20:32:00 +0000

    added ansible-lint and linted all ansible related files

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-state-gonzaloarce"
     key            = "neogenomics-devops-test/ec2.tfstate"
-    region         = var.region
+    region         = "eu-north-1"
     dynamodb_table = "my-tf-lock"
     encrypt        = true
   }
